6a477b5e6f1960e845e85924e4530b9065e5d3ec
jbdevelop3r <justinebanogon14@gmail.com>
2025-07-11 18:36:14 +0800

    added api call

--- app/wrappers/ava_api.rb
+++ app/wrappers/ava_api.rb
@@ -4,12 +4,11 @@ require 'net/http'
 class AvaApi
   def self.fetch_records(symbol)
     url = URI("https://alpha-vantage.p.rapidapi.com/query?datatype=json&output_size=compact&interval=5min&function=TIME_SERIES_INTRADAY&symbol=#{symbol}")
-
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-key"] = ENV["API_KEY"]
+    request["x-rapidapi-key"] = "91b14244camsh1783fa012f5731cp10dde2jsn9ee8a32de7fa"
     request["x-rapidapi-host"] = "alpha-vantage.p.rapidapi.com"
 
     response = http.request(request)
